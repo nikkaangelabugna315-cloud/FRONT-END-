f5088306017def77bd55ebb6cb226d8c9443d89a
nikka <nikkaangelabugna315@gmail.com>
2025-10-11 17:53:04 +0800

    lets do this!!

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Layout from "./Layout";
 import AnotherPage from "./pages/dashboard/AnotherPage";
+import Students from "./pages/students/Students";
+import Course from "./pages/course/Course";
+import Task from "./pages/task/Task";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -9,6 +12,10 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Dashboard /> },
       { path: "another-page", element: <AnotherPage /> },
+      { path: "students", element: <Students /> },
+      { path: "course", element: <Course/> },
+      { path: "task", element: <Task/> },
+
     ],
   },
 ]);
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -16,6 +16,15 @@ function Aside() {
           <li>
             <NavLink to="/another-page">Another Page</NavLink>
           </li>
+          <li>
+            <NavLink to="students">Students</NavLink>
+          </li>
+          <li>
+            <NavLink to="course">Course</NavLink>
+          </li>
+          <li>
+            <NavLink to="task">Task</NavLink>
+          </li>
         </ul>
       </div>
     </div>
